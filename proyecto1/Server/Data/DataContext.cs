f7406864d691158a7c8c524d84962cd305c3ec4d
INER8 <INER8@INER8-PC>
2024-04-10 13:57:39 +0200

    Add project files.

---
+++ proyecto1/Server/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿namespace proyecto1.Server.Data
+{
+	public class DataContext : DbContext
+	{
+        public DataContext(DbContextOptions<DataContext>options): base(options)
+        {
+            
+        }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Title = "Classic Rubik's Cube",
+					Description = "The Rubik's Cube is a three-dimensional mechanical puzzle created by Hungarian sculptor and architecture professor Ernő Rubik in 1974.",
+					Image = "./media/cubo1.png",
+					Price = 9.99m
+				},
+				new Product
+				{
+					Id = 2,
+					Title = "Pyraminx",
+					Description = "The Pyraminx is a mechanical puzzle shaped like a tetrahedron similar to a Rubik's cube. It was invented by Uwe Meffert in 1970.",
+					Image = "./media/cubo2.jpg",
+					Price = 7.99m
+				},
+				new Product
+				{
+					Id = 3,
+					Title = "Skewb",
+					Description = "The Skewb is a three-dimensional mechanical puzzle like a Rubik's cube, made up of pieces that can rotate and change position.",
+					Image = "./media/cubo3.jpg",
+					Price = 9.99m
+				});
+				}
+
+		public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ proyecto1/Server/Migrations/20240410113035_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyecto1.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "Price", "Title" },
+                values: new object[] { 1, "The Rubik's Cube is a three-dimensional mechanical puzzle created by Hungarian sculptor and architecture professor Ernő Rubik in 1974.", "./media/cubo1.png", 9.99m, "Classic Rubik's Cube" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "Price", "Title" },
+                values: new object[] { 2, "The Pyraminx is a mechanical puzzle shaped like a tetrahedron similar to a Rubik's cube. It was invented by Uwe Meffert in 1970.", "./media/cubo2.jpg", 7.99m, "Pyraminx" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "Price", "Title" },
+                values: new object[] { 3, "The Skewb is a three-dimensional mechanical puzzle like a Rubik's cube, made up of pieces that can rotate and change position.", "./media/cubo3.jpg", 9.99m, "Skewb" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
